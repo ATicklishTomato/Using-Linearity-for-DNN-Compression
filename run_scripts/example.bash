734e9b6bffa35fc0109c09ee30cc2619a0e187cd
Luuk Wubben <luukwubben2@gmail.com>
2026-02-04 19:51:45 +0100

    Cluster MIG doesnt have enough memory

--- run_scripts/example.bash
+++ run_scripts/example.bash
@@ -2,13 +2,14 @@
 
 #SBATCH --job-name=my_job
 #SBATCH --output=my_job_output_%j.txt
-#SBATCH --partition=tue.gpu.q         # Choose a partition that has GPUs
+#SBATCH --partition tue.gpu2.q
+#SBATCH --gres=gpu:l4.22gb:1
+#SBATCH --exclusive
 #SBATCH --time=16:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=2G
-#SBATCH --gpus=1                      # This is how to request a GPU
 
 # Load modules or software if needed optimized for GPU use if available
 # In the example PyTorch is made available for import in to my_sript.py
