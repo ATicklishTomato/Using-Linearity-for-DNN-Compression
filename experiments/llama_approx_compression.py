Fix Llama bugs and do successful run. Accuracy calculation wrong

experiments/llama_approx_compression.py
@@ -9,6 +9,7 @@ from utils.data_manager import DataManager
 from utils.llama_model import LlamaExperimenter
 
 logger = logging.getLogger(__name__)
+debug_mode = logger.getEffectiveLevel() != logging.DEBUG
 
 def group_contiguous_layers(linear_layers):
     """
@@ -117,11 +118,11 @@ def train_block_approximation(
     loss_fn = torch.nn.MSELoss()
     scaler = torch.amp.GradScaler(device)
 
-    model.eval()
-    approx.train()
+    model.eval().to(device)
+    approx.train().to(device)
 
     for epoch in range(epochs):
-        for i, batch in tqdm(enumerate(train_dataset), total=min(len(train_dataset), max_batches), desc=f"Training block {layer_group} Epoch {epoch}", leave=False):
+        for i, batch in tqdm(enumerate(train_dataset), total=min(len(train_dataset), max_batches), desc=f"Training block {layer_group} Epoch {epoch}", leave=False, disable=debug_mode):
             if i >= max_batches:
                 break
 
@@ -163,12 +164,12 @@ def train_block_approximation(
     return approx
 
 
-def train_approximation_layers(experimenter, train_dataset, groups, save_model: bool,
+def train_approximation_layers(experimenter, data_handler, groups, save_model: bool,
                                epochs: int, lr: float, max_batches: int, device: str):
     """Train linear approximations for specified layer groups in the model.
     Args:
         experimenter: The LlamaExperimenter instance containing the model to be compressed and its tokenizer.
-        train_dataset: The training dataset.
+        data_handler: The DataHandler instance containing the dataset to be compressed and its tokenizer.
         groups: List of layer groups to approximate.
         save_model (bool): Whether to save the compressed model to disk.
         epochs (int): Number of epochs to train.
@@ -184,9 +185,9 @@ def train_approximation_layers(experimenter, train_dataset, groups, save_model: 
             logger.info(f"Training approximation for layer group: {layer_group}")
             linear_block = train_block_approximation(
                 experimenter.model,
-                experimenter.tokenizer,
+                data_handler.tokenizer,
                 layer_group,
-                train_dataset,
+                data_handler.train_set,
                 device,
                 epochs=epochs,
                 lr=lr,
@@ -252,7 +253,7 @@ def run_experiment(model: str, linearity: str, dataset: str, threshold: str, bat
     # Group contiguous linear layers and create linear approximation layers
     # ------------------------------------------------------------
     groups = group_contiguous_layers(linear_layers)
-    train_approximation_layers(experimenter, data_handler.train_set, groups, save_model=save,
+    train_approximation_layers(experimenter, data_handler, groups, save_model=save,
                                epochs=epochs, lr=lr, max_batches=max_batches, device=device)
     logger.info("Linear approximation layers trained and integrated into the model.")
 

metrics/linearity_metrics.py
@@ -7,6 +7,7 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 logger = logging.getLogger(__name__)
+debug_mode = logger.getEffectiveLevel() != logging.DEBUG
 
 class LinearityMetric:
 
@@ -157,10 +158,11 @@ def mean_preactivations_llama(model, tokenizer, dataset, max_batches=30, device=
         num_batches = min(max_batches, len(dataset))
     # Forward pass through the data
     with torch.no_grad():
-        for i in tqdm(range(num_batches), desc="Processing samples for preactivations", leave=False):
+        for i in tqdm(range(num_batches), desc="Processing samples for preactivations", leave=False, disable=debug_mode):
             inputs = tokenizer(dataset[i]['text'], return_tensors='pt', truncation=True, padding='max_length', max_length=512)
             inputs = {k: v.to(device) for k, v in inputs.items()}
-            model(**inputs)
+            with torch.autocast("cuda", dtype=torch.bfloat16):
+                model(**inputs)
     logger.debug("Forward passes complete. Computing mean preactivations...")
 
     # Compute mean preactivations
@@ -248,7 +250,7 @@ def mean_preactivations_resnet(model, dataset, batch_size=1, device='cuda', save
     # Register hooks on BatchNorm layers (pre-ReLU in ResNet18)
     for name, module in tqdm(model.named_modules(),
                              desc="Registering hooks",
-                             leave=False):
+                             leave=False, disable=debug_mode):
         if isinstance(module, torch.nn.BatchNorm2d):
             hooks.append(module.register_forward_hook(get_hook(name)))
 
@@ -258,7 +260,7 @@ def mean_preactivations_resnet(model, dataset, batch_size=1, device='cuda', save
     with torch.no_grad():
         for inputs, _ in tqdm(data_loader,
                               desc="Computing mean preactivations",
-                              leave=False):
+                              leave=False, disable=debug_mode):
             inputs = inputs.to(device)
             model(inputs)
 

utils/llama_model.py
@@ -4,11 +4,13 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 import logging
-from transformers import LlamaForCausalLM, LlamaTokenizer
+from transformers import LlamaForCausalLM
+from transformers.utils.logging import disable_progress_bar
 
 from tqdm import tqdm
 
 logger = logging.getLogger(__name__)
+debug_mode = logger.getEffectiveLevel() != logging.DEBUG
 
 class LlamaExperimenter:
     def __init__(self, model_name, data_handler, batch_size, epochs, learning_rate, max_batches=None, device='cuda', save=False):
@@ -37,7 +39,10 @@ class LlamaExperimenter:
 
     def _initialize_llama_model(self, model_path):
         """Initialize a LLaMA model with the specified path."""
-        model = LlamaForCausalLM.from_pretrained(model_path, torch_dtype=torch.float16).to(self.device)
+        if logger.getEffectiveLevel() != logging.DEBUG:
+            disable_progress_bar()
+        model = LlamaForCausalLM.from_pretrained(model_path).to(self.device)
+        model.config.pad_token_id = self.data_handler.tokenizer.eos_token_id
         logger.info(f"Initialized LLaMA model from {model_path}.")
         return model
 
@@ -47,25 +52,27 @@ class LlamaExperimenter:
         self.model.to(self.device).train()
 
         optimizer = optim.AdamW(self.model.parameters(), lr=self.learning_rate)
-        loss_fn = nn.CrossEntropyLoss(ignore_index=self.data_handler.tokenizer.pad_token_id)
 
         train_loader = DataLoader(self.data_handler.train_set, batch_size=self.batch_size, shuffle=True)
         for epoch in range(self.epochs):
             epoch_loss = 0.0
             batch_idx = 0
-            for batch_idx, batch in enumerate(tqdm(train_loader, desc=f"Epoch {epoch+1}/{self.epochs}", total=min(self.max_batches, len(train_loader)), leave=False)):
+            for batch_idx, batch in enumerate(tqdm(train_loader, desc=f"Epoch {epoch+1}/{self.epochs}", total=min(self.max_batches, len(train_loader)), leave=False, disable=debug_mode)):
                 if batch_idx >= self.max_batches:
                     break
 
+                optimizer.zero_grad()
+
                 inputs = self.data_handler.tokenizer(batch['text'], return_tensors='pt', padding=True, truncation=True).to(self.device)
                 labels = inputs.input_ids.clone()
-                outputs = self.model(**inputs, labels=labels)
-                logits = outputs.logits
+                labels[labels == self.data_handler.tokenizer.pad_token_id] = -100
 
-                loss = loss_fn(logits.view(-1, logits.size(-1)), labels.view(-1))
+                with torch.autocast("cuda", dtype=torch.bfloat16):
+                    outputs = self.model(**inputs, labels=labels)
+                    loss = outputs.loss
 
-                optimizer.zero_grad()
                 loss.backward()
+                nn.utils.clip_grad_norm_(self.model.parameters(), 1.0)
                 optimizer.step()
 
                 epoch_loss += loss.item()
@@ -91,7 +98,7 @@ class LlamaExperimenter:
         val_loader = DataLoader(self.data_handler.val_set, batch_size=self.batch_size, shuffle=False)
         num_batches = min(self.max_batches, len(val_loader))
         with torch.no_grad():
-            for batch_idx, batch in enumerate(tqdm(val_loader, total=num_batches, desc="Validating LLaMA model", leave=False)):
+            for batch_idx, batch in enumerate(tqdm(val_loader, total=num_batches, desc="Validating LLaMA model", leave=False, disable=debug_mode)):
                 if batch_idx >= self.max_batches:
                     break
 
@@ -99,7 +106,8 @@ class LlamaExperimenter:
                 labels = inputs.input_ids.clone()
 
                 start_time = time.time()
-                outputs = model(**inputs)
+                with torch.autocast("cuda", dtype=torch.bfloat16):
+                    outputs = model(**inputs)
                 end_time = time.time()
                 inference_time += (end_time - start_time)
 

utils/resnet_model.py
@@ -9,6 +9,7 @@ import logging
 from tqdm import tqdm
 
 logger = logging.getLogger(__name__)
+debug_mode = logger.getEffectiveLevel() != logging.DEBUG
 
 class ResNetExperimenter:
     def __init__(self, model_name, data_handler, batch_size, epochs, learning_rate, max_batches=None, device='cuda', save=False):
@@ -61,7 +62,7 @@ class ResNetExperimenter:
         for epoch in range(self.epochs):
             running_loss = 0.0
             for i, data in tqdm(enumerate(train_loader), total=min(len(train_loader), self.max_batches),
-                                desc=f"Finetuning Epoch {epoch+1}/{self.epochs}", leave=False):
+                                desc=f"Finetuning Epoch {epoch+1}/{self.epochs}", leave=False, disable=debug_mode):
                 if i >= self.max_batches:
                     break
                 inputs, labels = data
@@ -94,7 +95,7 @@ class ResNetExperimenter:
         data_loader = DataLoader(self.data_handler.val_set, batch_size=self.batch_size, shuffle=False)
         num_batches = min(self.max_batches, len(data_loader))
         with torch.no_grad():
-            for inputs, labels in tqdm(data_loader, total=num_batches, desc="Validating ResNet model", leave=False):
+            for inputs, labels in tqdm(data_loader, total=num_batches, desc="Validating ResNet model", leave=False, disable=debug_mode):
                 if total >= self.max_batches * self.batch_size:
                     break
                 inputs = inputs.to(self.device)